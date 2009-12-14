b941625b05ff52335f079a5f529c687f59fe4dbc
Kevin Lipe <kevin@deadllama.net>
2009-12-14 02:49:33 -0600

    still rearranging

--- src/sigmsgs.h
+++
@@ -1,9 +0,0 @@
-/* sigmsgs.h -- interface to signal name and message date ($Revision: 1.1.1.1 $) */
-
-typedef struct {
-	int sig;
-	const char *name, *msg;
-} Sigmsgs;
-extern const Sigmsgs signals[];
-
-extern const int nsignals;
